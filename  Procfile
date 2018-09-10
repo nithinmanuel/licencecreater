20e7f34f0be12cea094577782b9f9ea3d21b2ef1
nithinmanuel <nithinmanuel9@gmail.com>
2018-09-10 03:35:02 +0200

    nithinmanuel

---
+++  Procfile
@@ -0,0 +1,1 @@
+web: gunicorn licencecreater.wsgi
--- oursite/settings.py
+++ oursite/settings.py
@@ -73,23 +73,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'oursite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-# DATABASES = {
-#'default': {
-#'ENGINE': 'django.db.backends.sqlite3',
-#'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#}
-#}
-
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default='sqlite:////{0}'.format(
+                os.path.join(BASE_DIR, 'db.sqlite3'))
+    )
 }
+
+
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
+#}
 
 
 # Internationalization
