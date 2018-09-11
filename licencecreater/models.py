798f8b1a39a7f935b70ca9e58122c83e5b02590c
nithinmanuel <39334292+nithinmanuel@users.noreply.github.com>
2018-09-11 03:09:10 +0200

    Update models.py

--- licencecreater/models.py
+++ licencecreater/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 from django.utils import timezone
-
+# the path to upload file
 
 def get_uploaded_document_path(instance, filename):
 
     name = 'uploaded/%s' % (filename)
     return name
-
+# the path of the converted file
 
 def get_converted_document_path(instance, filename):
 
     name = 'converted/%s' % (filename)
     return name
 
-
+# constructing a document class with attributes and the attributes calling a function which is the path of uploaded, converted file
 class Document(models.Model):
     uploaded = models.FileField(
         upload_to=get_uploaded_document_path, default=None)
@@ -27,7 +27,7 @@ class Document(models.Model):
 
 def __str__(self):
     return '%s %s' % (self.timestamp, self.id)
-
+# the words class to here we are constructing a class with the words need to be change in the uploaded file 
 
 class Keyword(models.Model):
     name = models.CharField(max_length=250, default=None)
