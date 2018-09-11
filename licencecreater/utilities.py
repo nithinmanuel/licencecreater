f62849979c78cdbe3efccd1349cad87acc5eec1a
nithinmanuel <39334292+nithinmanuel@users.noreply.github.com>
2018-09-11 07:09:41 +0200

    Update utilities.py

--- licencecreater/utilities.py
+++ licencecreater/utilities.py
@@ -5,7 +5,13 @@ from .models import Document
 from pathlib import Path
 
 from django.core.files import File
-
+# the operation starts in another function known as uploaded the follow up is to first to read the file contant from uploaded  
+# in the line 12 carry out this task . The line 15 pulls out all the objects in the databse and next line is a for loop iterating
+# the keywords containing the words from databse the words only picking up the name of the words not the id which is unwanted 
+# so the words will have the name of all words , after this again one more for loop where file_content.replace will replace the 
+# word with '®' which will apply to all words in the for loop .last one is to return the edited filecontent will goes up to the
+# upldoaded function .
+# 
 
 def convert_file(file):
     file_content = file.read().decode("utf-8-sig")
@@ -15,8 +21,7 @@ def convert_file(file):
     words = [o.name for o in keywords]
     print(words)
 
-    #replace_word = [s + '®' for s in words]
-    #print(replace_word)
+    
 
     for word in words:
 
@@ -24,54 +29,4 @@ def convert_file(file):
             word, word + '®')
     print(file_content)
     return (file_content)
-    #f = open('/home/nithin/oursite/media/uploaded/ola', 'r+')
-    #converted_file = File(f)
-    # converted_file.write(file_content)
-    # converted_file.closed
-    # return (converted_file)
-
-    #obj = Document.objects.get(uploaded="yes")
-    # print(obj)
-    # filer = open(os.path.join(BASE_DIR, 'media\\uploaded', converted)
-    #data_folder = Path("MEDIA_ROOT/uploaded")
-
-    #file_to_open = data_folder / "yes.txt"
-    #contents = open(str(yes), "r").read()
-    #f = open(file_to_open)
-
-    # f.close()
-    # print(data)
-    # print(file_content)
-    # try:
-    # ty = Document.objects.get(id=10, uploaded='converted')
-    # except Document.DoesNotExist:
-    # ty = None
-    # print(ty)
-    # obj = Document.objects.get(uploaded = 'converted')
-
-    # print(obj)
-    # tye = ty.read().decode("utf-8-sig")
-    # tye.write(file_content)
-    # print(tye)
-    #converted_file = open(file.path, 'converted', 'w')
-    # converted_file.write(file_content)
-    # file_use = converted_file.read()#.decode("utf-8-sig")
-    # print(converted_file)
-    # converted_file.close()
-    # response = convert_file.read()
-    # print(response)
-    # return (converted_file)
-    # fp = open('file')
-    # data = fp.read().decode("utf-8-sig")
-    # print(data)
-
-    # file_wse = file.open()
-    # rt = open('file_we', 'r')
-# print (file_content)
-# ss ivide words le word fileisl undo ennu
-    # print(keyswords) str(word) + str('®')
-    # ds = keywords.values()  + ['®']
-    # words = ds.values()
-    # print(words)
-    # g = bytes('®', encoding="UTF-8")
-    # print(g)
+    
