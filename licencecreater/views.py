7459eba4aa9ba5307a6d812f23b7d65864c24b09
nithinmanuel <39334292+nithinmanuel@users.noreply.github.com>
2018-09-11 06:43:57 +0200

    Update views.py

--- licencecreater/views.py
+++ licencecreater/views.py
@@ -9,7 +9,12 @@ import json
 from django.core import serializers
 import os
 from django.core.files import File
-
+# by post method we will get the file using the if request.method and it is assign to document variable twice 
+# using the if condition first to check wheather the file is in the request and second is to check the file is a txt file
+# if it has file and it is atxt file the it prevails else fails and invoke status 400. The file is need to store in the databse
+# for this reason the file is creating in line 33 , here the document is passing through one of the attributies of the class document
+# and the file is created as an object in databse . for every object created in the database has a unique id(feteching it in line 40) , this id is need 
+# to be returned as a response and in line 43 it depics to how to return in as a json resposne.
 
 def upload_document(request):
     # we use Files because if it is media files then it's a get
@@ -30,7 +35,7 @@ def upload_document(request):
     file = Document.objects.create(
         uploaded=document)  # defenition of the object
     file.save()  # created
-    # this is a dictinary and it is used to respon
+    # this is a dictinary and it is used to response
 
     response_data = {'id': file.id}
 
@@ -38,6 +43,12 @@ def upload_document(request):
     response = json.dumps(response_data)
     return HttpResponse(response, content_type='json')
 
+
+# The id of the document is need to perform the appending the trademark symbol , firstly calling the get function
+# using the try and except and if the document id is in the GET REQUEST  then it pass to a function in line 57 ELSE 404.
+# The id is passing to a function which is responsbile for the operation to append the symbol and the file_content will
+# have the modified file content and this modified file content gives as reponse in json format.(line 61) ? the missing parts 
+# the document need to save in the converted file it is on the way..
 
 def download_converted_document(request, document_id):
     try:
@@ -48,13 +59,19 @@ def download_converted_document(request, document_id):
 
     file_content = convert_file(document.uploaded)
     print(file_content)
-    response = json.dumps(file_content)
+    response = json.dumps(file_content)    # the resposne is need to create as an object 
     print(response)
     return HttpResponse(response, content_type='json')
 
-    # this is need to be converted into a file s
+ # To create the a set of words that is to undergo the transformation which is mentioned above . the word need to add to the
+   # database should pass in json format ( line 75) receving the data as post and decoding the encoded format proceed by assign
+# it to a a varible in line 77. like previous cases checking that wheather the POST request lands in the funcrion create_keyword
+# contains word or something else and preceeed only the request carries the word. line number 82 is to create the object as it 
+# happens through the one and only attribute of the selected word need to face the alternation. As it stated above each object
+# in the database has a distinctive id which is required to be enclosed in the json response and the lines 85, 87 performing this art.
 
-
+    
+    
 def create_keyword(request):  # create keyword
     # if the requst is empty means there is no data is send then give 404
     print (request.body)
@@ -68,6 +85,12 @@ def create_keyword(request):  # create keyword
 
     response = json.dumps(response_data)
     return HttpResponse(response, content_type='json')
+
+# A GET Request to get all words which is created in database through json. it initaites with a GET request and it is passing
+# to a varibale in line 94 . next line is for to get all the words in databse(97 line) . line number 100 calling a for loop
+# and inside this for loop iterating the each and every word that is allready in the databse and pick up the id and the name 
+# of the keyword guides to the dictionary declared in line 101 , storing the data from line 103 into line 101 dictionary. Rest 
+# of the operation is same in the previous cases.
 
 
 def get_keywords(request):  # list of keywords
@@ -98,6 +121,10 @@ def get_keywords(request):  # list of keywords
     #response_data = {'id': keywords.id, 'name': keyword.name}
     # return(json.dumps(response_data))
 
+    
+# A GET request to get a single word from the database for this the request comes with the id if the word which is want to return
+# as a json response. In line 132 makes the job easier by collecting the requied object and this object is pass to the variable in the asme line
+# next actions are riping the atributes of the object and send convert and send it as json response.
 
 def single_keyword(request, keyword_id):
  # get one keyword
@@ -108,6 +135,10 @@ def single_keyword(request, keyword_id):
     response = json.dumps(response_data)
     return HttpResponse(response, content_type='json')
 
+# This operation is to replace a word which is no longer wanted as word that is supposed to modify inside the file . it is a 
+# POST operation carries the id of the word need to change not only this but also brings the new word to be in place of the word that will undergo a change . 
+# Thanks to get_object method to find the object supposed to substitute line 148 performs the operation of replacing the words 
+# and it will deleiver a sucess message
 
 def edit_keyword(request, keyword_id):
 
@@ -119,6 +150,9 @@ def edit_keyword(request, keyword_id):
     element.save()
     return HttpResponse()
 
+# the delete operation is to delete the particular word from the database and it is get request contains the id of the word 
+# want to delete which will get through get_object operation in the line 160 and the following lines performs the actions to delete
+# in line 162. finaly returns a http response.
 
 def delete_keyword(request, keyword_id):
 
